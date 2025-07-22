c603c14ef62a41ef2f8c37d40e4059659c5193c3
anuj308 <anujkumarsharma2023@gmail.com>
2025-07-22 12:56:13 +0530

    added user controller like signup login logout , and middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import UserRouter from "./routes/user.route.js"
-
-const app = express()
+import UserRouter from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
+const app = express();
 const port = process.env.PORT;
 
 dotenv.config({
-    path:"../.env"
+  path: "../.env",
 });
 
 connectDB();
 
-app.use("/api/v1/auth",UserRouter);
+app.use(express.json());
+app.use(cookieParser());
 
+app.use("/api/auth", UserRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
